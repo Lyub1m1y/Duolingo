175cc0096373ca97dc9cae6a432a4a42b6462dba
Lyub1m1y <dokhoyan-leva@mail.ru>
2023-04-15 23:01:14 +0400

    Added translation using Lecto AI API (need to replace with another API)

--- src/main/java/org/duolingo/Main.java
+++ src/main/java/org/duolingo/Main.java
@@ -1,5 +1,10 @@
 package org.duolingo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.InputStreamReader;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -11,13 +16,16 @@ import java.io.IOException;
 
 public class Main {
 
-  private static final String FILE_PATH = "src/main/resources/login_info.txt";
+  private static final String FILE_PATH = "src/main/resources/data.txt";
 
-  public static void main(String[] args) {
+  private static StringBuilder login = new StringBuilder();
+  private static StringBuilder password = new StringBuilder();
+  private static StringBuilder keyApi = new StringBuilder();
+
+  public static void main(String[] args) throws IOException {
     WebDriver driver = new FirefoxDriver();
     driver.get("https://www.duolingo.com/");
 
-//    driver.findElements(By.ByXPath.xpath("".get));
     loggingInAccount(driver);
 
     while (true) {
@@ -29,15 +37,13 @@ public class Main {
 
   public static void loggingInAccount(WebDriver driver) {
     try {
-      // Click the "Log in" button
+      // Open login page
       driver.findElement(
               By.ByXPath.xpath("/html/body/div[1]/div[1]/div/div[2]/div[1]/div[2]/div[2]/button"))
           .click();
 
       // Enter login credentials
-      StringBuilder login = new StringBuilder();
-      StringBuilder password = new StringBuilder();
-      readFileData(login, password);
+      readFileData(login, password, keyApi);
       driver.findElement(By.ByXPath.xpath(
               "/html/body/div[2]/div[2]/div/div/form/div[1]/div[1]/div[1]/label/div/input"))
           .sendKeys(login);
@@ -54,19 +60,18 @@ public class Main {
     }
   }
 
-  public static void readFileData(StringBuilder login, StringBuilder password) {
-    System.out.println("----------hui-------------");
-    System.out.println(System.getProperty("user.dir"));
-    System.out.println(FILE_PATH);
-    System.out.println("----------hui-------------");
+  public static void readFileData(StringBuilder login, StringBuilder password,
+      StringBuilder keyApi) {
     try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
       String line;
       int lineCount = 1;
-      while ((line = reader.readLine()) != null && lineCount != 5) {
+      while ((line = reader.readLine()) != null && lineCount != 7) {
         if (lineCount == 2) {
           login.append(line);
         } else if (lineCount == 4) {
           password.append(line);
+        } else if (lineCount == 6) {
+          keyApi.append(line);
         }
         lineCount++;
       }
@@ -120,19 +125,64 @@ public class Main {
     }
   }
 
-  public static void passingLevel(WebDriver driver) {
+  public static void passingLevel(WebDriver driver) throws IOException {
     // Task 1
-    answer(driver, "Good");
-    // Task 2
-    answer(driver, "Good");
-    // Task 3
-    answer(driver, "Good thanks");
-    // Task 4
-    answer(driver, "Goodbye");
-    // Task 5
-    answer(driver, "Goodbye");
-    // Switch task 5
-    answer(driver, "Good and you?");
+    String answer;
+    for (int i = 0; i < 5; i++) {
+      answer(driver, translate(driver.findElement(By.xpath(
+              "/html/body/div[1]/div[1]/div/div/div[2]/div/div/div/div/div[2]/div[1]/div/div[2]/div[1]/div/span"))
+          .getText()));
+    }
+  }
+
+  public static String translate(String input) throws IOException {
+    String to = "en";
+    String from = "ru";
+    String url = "https://api.lecto.ai/v1/translate/text";
+
+    String[] cmd = {"curl", "-X", "POST", "-H", "X-API-Key: " + keyApi, "-H",
+        "Content-Type: application/json", "-H",
+        "Accept: application/json", "--data-raw",
+        "{\"texts\": " + toJson(input) + ", \"to\": " + toJson(to) + ", \"from\": \"" + from
+            + "\"}",
+        url};
+
+    ProcessBuilder pb = new ProcessBuilder(cmd);
+    Process p = pb.start();
+    BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+    String line;
+    StringBuilder response = new StringBuilder();
+    while ((line = reader.readLine()) != null) {
+      response.append(line);
+    }
+
+    System.out.println("-------input---------");
+    System.out.println(input);
+    System.out.println("-------input---------");
+    System.out.println("-------json---------");
+    System.out.println(response.toString());
+    System.out.println("-------json---------");
+    System.out.println("-------answer---------");
+    String an = getAnswer(response.toString());
+    System.out.println(an);
+    System.out.println("-------anwer---------");
+    return an;
+  }
+
+  private static String getAnswer(String json) throws JsonProcessingException {
+    ObjectMapper objectMapper = new ObjectMapper();
+    JsonNode rootNode = objectMapper.readTree(json);
+    return rootNode.get("translations").get(0).get("translated").get(0).asText();
+  }
+
+  private static String toJson(String array) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("[\"");
+    sb.append(array
+    );
+    sb.append("\"]");
+    return sb.toString();
   }
 
   public static void answer(WebDriver driver, String answer) {
