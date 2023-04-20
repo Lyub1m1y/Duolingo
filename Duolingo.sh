6b55af36ae0eab9b77e41e688f45d40ac4ca2b06
Lyub1m1y <dokhoyan-leva@mail.ru>
2023-04-20 14:11:12 +0400

    fixed Duolingo.sh and removed unnecessary dependencies in pom.xml

--- Duolingo.sh
+++ Duolingo.sh
@@ -2,4 +2,4 @@
 # this sh is needed to run the program
 
 cd  $HOME/Desktop/Duolingo/
-mvn exec:java -Dexec.mainClass="org.duolingo.Main"
+mvn clean compile exec:java -Dexec.mainClass="org.duolingo.Main"
